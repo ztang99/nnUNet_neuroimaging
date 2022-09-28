05ff9f9cce8d7f48aa49265d3e54412bf6a95454
Zitian (Chloe) Tang <112351224+ztang99@users.noreply.github.com>
2022-09-28 13:53:15 -0500

    updated trainers & scripts

---
+++ scripts/train_nnunet_T1c.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+######## Job Name: Test_Job ########
+#SBATCH -J Train_SEG_Job_T1c_only
+#SBATCH -o logs/Train_SEG_Job_T1c_only.o%j
+#SBATCH -e logs/Train_SEG_Job_T1c_only.e%j
+######## Number of nodes: 1 ########
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task 10
+#SBATCH --gres gpu:tesla_a100:1
+#SBATCH --mem 64G 
+#SBATCH -t 48:00:00
+
+cd /home/tang.zitian/nnUNet
+# run script: sbatch scripts/train_nnunet_T1c.sh
+
+######## Load module environment required for the job ########
+module load cuda/11.3
+source activate base
+
+nnunet_use_progress_bar=1 nnUNet_n_proc_DA=10 nnUNet_train 3d_fullres nnUNetTrainer_T1c Task101_BraTS2021 1 --npz -p nnUNetPlansv2.1_verybig
+
